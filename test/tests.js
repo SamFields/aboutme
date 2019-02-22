61c4124c9d5bb1889771fc6a41b9cd47a2151822
Sam Fields <42815701+SamFields@users.noreply.github.com>
2019-02-22 10:57:45 -0600

    Did some stuff
    Almost completed unitconv.html

--- test/tests.js
+++ test/tests.js
@@ -7,6 +7,10 @@ function calculate(temp) {
     return numb
 }
 
+QUnit.test("Here's a test that should always pass", function (assert) {
+    assert.ok(1 == "1", "1=1 success!");
+});
+
 QUnit.test('Testing conversion function with positive inputs', function (assert) {
     assert.equal(parseFloat(Math.round(calculate(15) * 100) / 100).toFixed(2), -9.44, 'Tested with 15');
     assert.equal(parseFloat(Math.round(calculate(32) * 100) / 100).toFixed(2), 0, 'Tested with 32');
@@ -23,6 +27,6 @@ QUnit.test('Testing conversion function with 0', function (assert) {
     assert.equal(parseFloat(Math.round(calculate(0) * 100) / 100).toFixed(2), -17.78, 'Tested with 0');
 });
 
-// QUnit.test('Testing conversion function with non-numbers', function (assert) {
-// 
-// });
+QUnit.test('Testing conversion function with non-numbers', function (assert) {
+    assert.throws(function () { calculate("a"); }, /The given argument is not a number/, 'Passing in a string correctly raises an Error');
+});
